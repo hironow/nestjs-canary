5ba646567c9f94e0dda611173dc78c3c3dd8e97b
N. Hiroto <hironow365@gmail.com>
2022-10-02 02:21:49 +0900

    Add scenario test use runn cli and loadtest use k6 (#2)

---
+++ script/loadtest.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -eu
+
+# mv dir
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly SCRIPT_DIR
+cd "${SCRIPT_DIR}/.."
+
+# check cli
+k6 > /dev/null 2>&1 || (echo "ERROR: 'k6' must be installed and available on your PATH. please install ref. https://github.com/grafana/k6"; exit 1)
+
+# use k6 as a tool for loadtesting
+k6 run loadtest/app.loadtest-spec.js
