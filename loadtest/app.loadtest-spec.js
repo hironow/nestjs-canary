31f61053f2fbefb68f383618acd38928530b43ff
hironow <hironow365@gmail.com>
2022-10-02 02:20:10 +0900

    add loadtest using k6

---
+++ loadtest/app.loadtest-spec.js
@@ -0,0 +1,25 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+const base = 'http://localhost:3000';
+
+export default function () {
+  let query = `
+    query  {
+        sayHello
+    }`;
+
+  let headers = {
+    'Content-Type': 'application/json',
+  };
+
+  let res = http.post(`${base}/graphql`, JSON.stringify({ query: query }), {
+    headers: headers,
+  });
+
+  if (res.status === 200) {
+    console.log(JSON.stringify(res.body));
+  }
+
+  sleep(1);
+}
