1be44a00b13c7094ae9b1275371c9d9d130d8f6d
hironow <hironow365@gmail.com>
2022-10-06 23:59:09 +0900

    fix test

--- src/global-config/global-config.service.spec.ts
+++ src/global-config/global-config.service.spec.ts
@@ -1,15 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GlobalConfigService } from './global-config.service';
+import { ConfigModule } from '@nestjs/config';
+import { generateGlobalConfig } from './configuration';
 
 describe('GlobalConfigService', () => {
   let service: GlobalConfigService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          ignoreEnvFile: true,
+          load: [generateGlobalConfig],
+        }),
+      ],
       providers: [GlobalConfigService],
+      exports: [GlobalConfigService],
     }).compile();
 
-    service = module.get<GlobalConfigService>(GlobalConfigService);
+    service = await module.resolve<GlobalConfigService>(GlobalConfigService);
   });
 
   it('should be defined', () => {
@@ -17,6 +26,8 @@ describe('GlobalConfigService', () => {
   });
 
   describe('get', () => {
-    expect(service.get('gcp.projectId')).toBe('test');
+    it('call', () => {
+      expect(service.get('gcp.projectId')).toBe('test');
+    });
   });
 });
