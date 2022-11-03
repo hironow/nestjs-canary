3f545dadedd04e1ef3e3397ec5510e2b0e302f3c
N. Hiroto <hironow365@gmail.com>
2022-11-03 17:20:49 +0900

    Add load test github actions (#12)

--- loadtest/app.loadtest-spec.js
+++ loadtest/app.loadtest-spec.js
@@ -3,6 +3,16 @@ import { sleep } from 'k6';
 
 const base = 'http://localhost:3000';
 
+export const options = {
+  duration: '.5m',
+  vus: 5,
+  iterations: 10,
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
+  },
+};
+
 export default function () {
   let query = `
     query  {
--- script/loadtest.sh
+++ script/loadtest.sh
@@ -10,4 +10,4 @@ cd "${SCRIPT_DIR}/.."
 k6 > /dev/null 2>&1 || (echo "ERROR: 'k6' must be installed and available on your PATH. please install ref. https://github.com/grafana/k6"; exit 1)
 
 # use k6 as a tool for loadtesting
-k6 run loadtest/app.loadtest-spec.js
+k6 run loadtest/*.js
