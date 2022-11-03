1fd7d2fe24b3eff674793ea4b575a5b86e9bb335
hironow <hironow365@gmail.com>
2022-11-03 16:27:33 +0900

    fix html path

--- loadtest/app.loadtest-spec.js
+++ loadtest/app.loadtest-spec.js
@@ -6,7 +6,7 @@ const base = 'http://localhost:3000';
 
 export function handleSummary(data) {
   return {
-    './loadtest/summary.html': htmlReport(data),
+    'summary.html': htmlReport(data),
   };
 }
 
